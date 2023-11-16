5f41fdcb78fc31b34edc6535b6dda5732545d877
Grow-with-Talha <141271100+Grow-with-Talha@users.noreply.github.com>
2023-11-16 12:33:42 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { fetchDatafromApi } from "./utils/api";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer"
-import Home from "./pages/home/Home";
+import Home from "./pages/Home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
