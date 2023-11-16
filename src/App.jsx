94c95cd7169336315eb4c7c47e1c6bd211afece3
Talha Ali <alit83219@gmail.com>
2023-11-16 12:47:31 +0500

    aisay hi

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import { fetchDatafromApi } from "./utils/api";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer"
-import Home from "./pages/home/Home";
+import Home from "./pages/Home/Home";
 import Details from "./pages/details/Details";
-import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound"
+import 
 
 function App() {
   const { url } = useSelector((state) => state.home);
